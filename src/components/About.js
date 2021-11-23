e8873863438135e075957fde5137bd9ad57a12ab
Dayton <kirylmitch@gmail.com>
2021-11-23 13:44:43 -0500

    commit tree 396524cc30e8aa503107e7ea7138ef66d9bea22e

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
+function About(props) {
+  const condMake = props.bio
+  if (condMake) {
+    return (
+
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{condMake}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links git={props.github} link={props.linkedin} />
     </div>
-  );
+  )} 
+  (
+
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links git={props.github} link={props.linkedin} />
+    </div>
+  )
+
+  
 }
 
 export default About;
