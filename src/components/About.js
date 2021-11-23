fa51eb8df80ab084aaa929d2f864d03e7d6a9696
Dayton <kirylmitch@gmail.com>
2021-11-23 13:57:07 -0500

    it works

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const condMake = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {condMake ? <p>{condMake}</p> : ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links git={props.github} link={props.linkedin} />
     </div>
-  );
+  ) 
+  
+  
 }
 
 export default About;
